eeca398584c8021115f4c1e4fa9caae22ac8147f
Alvaro Segura <alvaro.segura@i3television.es>
2021-10-03 17:41:18 +0200

    updated javadoc of StatusService.java

--- src/main/java/com/example/twitter/service/status/StatusService.java
+++ src/main/java/com/example/twitter/service/status/StatusService.java
@@ -2,6 +2,11 @@ package com.example.twitter.service.status;
 
 import twitter4j.Status;
 
+/**
+ * This service its intended for working between Status and our DTO TweetDTO
+ * @author alvaro
+ *
+ */
 public interface StatusService {
 
 	void convertAndSaveStatus(Status status);
